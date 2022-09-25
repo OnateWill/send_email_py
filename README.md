d0effd51c917b5478d493b12180b0e5fcf44603f
Won <onatewill42@gmail.com>
2022-09-25 16:44:14 -0300

    Envia um e-mail simples com app Outlook

---
+++ README.md
@@ -0,0 +1,21 @@
+# send_email_simple
+
+Description.
+Send emails with app Outlook with Python.
+
+## Installing
+
+Use de pack manager [pip](https://pip.pypa.io/en/stable/) to install send_email_simple
+
+'''bash
+pip install package_name
+'''
+
+## Usage
+
+from package_name import enviar_email_com_python
+enviar_email_com_python.enviar_email()
+
+## Author
+
+William Onate
---
+++ send_email_simple/enviar_email_com_python.py
@@ -0,0 +1,30 @@
+import win32com.client as win32
+
+
+def enviar_email(destino: str, assunto: str, body: str):
+    """
+    Este código envia um e-mail simples, ou seja, sem anexos
+    Para utilizar este código, você deve ter o app do outlook em seu
+    windows.
+
+    :destino: coloque o endereço de email
+    :assunto: coloque sem assunto/subject
+    :body: coloque seu corpo do email
+    """
+
+    # criar a integração com o outlook
+    outlook = win32.Dispatch('outlook.application')
+
+    # criar um email
+    email = outlook.CreateItem(0)
+
+    # configurar as informações do seu e-mail
+    email.To = destino
+    email.Subject = assunto
+    email.HTMLBody = f"""
+    <p>{body}</p>
+
+    """
+
+    email.Send()
+    print("Email Enviado")
